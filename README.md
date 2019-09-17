138e8254cbae02b01073c3f59a6523875884b871
Creat1v <mmorpgz1337@gmail.com>
2019-09-17 23:16:06 +0200

    Added /weather command with API connection and other minor adjustments

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-# Telegram_bot
-First experience creating a telegram bot based on Python3 and python-telegram-bot (https://github.com/python-telegram-bot/python-telegram-bot) library.
+# Introduction
+First experience creating a telegram bot based on Python 3 and the [python-telegram-bot](https://github.com/python-telegram-bot/python-telegram-bot) library.
 
-# Requirements:
-* pip install python-telegram-bot --upgrade
-* Rename token_sample.yml to token.yml and adjust the token from your telegram bot.
+# Requirements
+* pip install python-telegram-bot pyyaml requests --upgrade
+* Telegram bot token
+* OpenWeatherMap API key
+
+# Installation
+* Rename token_sample.yml to token.yml and adjust the token/API keys.
--- main.py
+++ main.py
@@ -3,14 +3,15 @@
 Using Library of https://github.com/python-telegram-bot/python-telegram-bot
 https://github.com/python-telegram-bot/python-telegram-bot/wiki/Extensions-%E2%80%93-Your-first-Bot"""
 
-
 import logging
+import json
+import requests
 import yaml
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
-from telegram.ext import MessageHandler, Filters
 from telegram import InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import CommandHandler
 from telegram.ext import InlineQueryHandler
+from telegram.ext import MessageHandler, Filters
+from telegram.ext import Updater
 
 CONF = yaml.safe_load(open('token.yml'))
 
@@ -25,7 +26,10 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 def start(update, context):
     """Reaction to '/start' command messages"""
     context.bot.send_message(chat_id=update.message.chat_id,
-                             text="I'm DK_Bot, please talk to me!")
+                             text="""I'm dkpbot and still in development.
+Type /weather for current weather information in Vienna
+Type /start to see this message again
+Type @dkpbot <message> to try the "ALL-CAPS"-feature""")
 
 
 START_HANDLER = CommandHandler('start', start)
@@ -63,6 +67,31 @@ INLINE_CAPS_HANDLER = InlineQueryHandler(inline_caps)
 DISPATCHER.add_handler(INLINE_CAPS_HANDLER)
 
 
+def weather(update, context):
+    """Reaction to '/weather' command messages by requesting
+    weather data via API of OpenWeatherMap"""
+    url = "https://api.openweathermap.org/data/2.5/weather?"
+    data = {"id": "2761369", "units": "metric", "appid": CONF['api']['weather']}
+    response = requests.request("GET", url, params=data)
+    parsed_response = json.loads(response.text)
+    weather_message = """Weather in {}:
+Temp: {}°C
+Min/Max: {}°C/{}°C
+Humidity: {}%
+Wind speed: {}km/h""".format(parsed_response['name'],
+                             parsed_response['main']['temp'],
+                             parsed_response['main']['temp_min'],
+                             parsed_response['main']['temp_max'],
+                             parsed_response['main']['humidity'],
+                             parsed_response['wind']['speed'])
+    context.bot.send_message(chat_id=update.message.chat_id,
+                             text=weather_message)
+
+
+WEATHER_HANDLER = CommandHandler('weather', weather)
+DISPATCHER.add_handler(WEATHER_HANDLER)
+
+
 def unknown(update, context):
     """a MessageHandler with a command filter to reply to all commands
     that were not recognized by the previous handler"""
@@ -73,4 +102,6 @@ def unknown(update, context):
 UNKNOWN_HANDLER = MessageHandler(Filters.command, unknown)
 DISPATCHER.add_handler(UNKNOWN_HANDLER)
 
-#updater.stop()
+# Waits until one of the signals SIGINT, SIGTERM or SIGABRT are received and stops the updater
+# UPDATER.stop() will be called on receiving one of those signals.
+UPDATER.idle()
